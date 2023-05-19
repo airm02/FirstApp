2214eca009297aaa301be45b46b1aec9df3f3531
jiwoon <jwgil06@naver.com>
2023-05-19 12:27:04 +0900

    s

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -1,33 +1,75 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
 //
-//  ContentView.swift
-//  FirstApp
-//
-//  Created by 권아림 on 2023/05/19.
-//
+//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
 
 import SwiftUI
 
+// MARK: - WelcomeElement
+
+var carrots: [Carrot] = load("dealData.json")
+
+func load<T: Decodable>(_ filename: String) -> T {
+    let data: Data
+
+    guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
+    else {
+        fatalError("Couldn't find \(filename) in main bundle.")
+    }
+
+    do {
+        data = try Data(contentsOf: file)
+    } catch {
+        fatalError("Couldn't load \(filename) from main bundle:\n\(error)")
+    }
+
+    do {
+        let decoder = JSONDecoder()
+        return try decoder.decode(T.self, from: data)
+    } catch {
+        fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
+    }
+}
+
 struct Carrot: Codable {
-    var createdTime : String
-    var post : Post
+    let createdTime: String
+    let post: Post
+    let author: Author
+    let product: Product
+    let isBooked, isPriceOfferAvailable: Bool
 }
-struct Post : Codable {
-    var title : String
-    var content : String
-    var reaction : Reaction
+
+// MARK: - Author
+struct Author: Codable {
+    let location: String
+    let profileImageURL: String
+    let nickname: String
+    let mannerTemperature: Double
 }
-struct Reaction : Codable {
-    var commentsCount : Int
-    var heartsCount : Int
+
+// MARK: - Post
+struct Post: Codable {
+    let title, content: String
+    let reaction: Reaction
 }
-struct Author : Codable {
-    var location :String
-    var profileImageURL: String
-    var nickname : String
-    var mannerTemperature : Int
+
+// MARK: - Reaction
+struct Reaction: Codable {
+    let commentsCount, heartsCount: Int
 }
-struct Product : Codable {
-    var price : Int
-    var category :String
-    var imageURLs :String
+
+// MARK: - Product
+struct Product: Codable {
+    let price: Int
+    let category: String
+    let imageURLs: [String]
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hi")
+            .onAppear {
+                print(carrots)
+            }
+    }
 }
