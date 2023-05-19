d477553a77a3949006cfc18f6cc30a4aa300da4f
Arim <dkflarnjs11@gmail.com>
2023-05-19 10:54:18 +0900

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by 권아림 on 2023/05/19.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
