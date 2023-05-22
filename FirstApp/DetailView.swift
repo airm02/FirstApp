79d980470f9df4985de36bb6d61305eb1b0ba2ec
Arim <dkflarnjs11@gmail.com>
2023-05-22 11:07:55 +0900

    라아아ㅏㅣㅇ

---
+++ FirstApp/DetailView.swift
@@ -0,0 +1,32 @@
+//
+//  DetailView.swift
+//  FirstApp
+//
+//  Created by 권아림 on 2023/05/22.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        List {
+            carrotRow()
+            carrotRow()
+            carrotRow()
+            carrotRow()
+            carrotRow()
+            carrotRow()
+            carrotRow()
+            carrotRow()
+            carrotRow()
+
+            
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
--- FirstApp/carrotRow.swift
+++ FirstApp/carrotRow.swift
@@ -8,23 +8,27 @@
 import SwiftUI
 
 struct carrotRow: View {
-    var carrot: Carrot
-
     var body: some View {
         
-        VStack(alignment:.leading){
-            Text("글 제목, 동네, 올린")
-            Text("시각, 금액")
+        
+        HStack{
+            Rectangle()
+                .frame(width: 130, height: 130)
+                .cornerRadius(10)
             
+                .padding()
+
+            VStack(alignment:.leading){
+                Text("글 제목, 동네, 올린")
+                Text("시각, 금액")
+            }
+            Spacer()
         }
-        
-        
     }
 }
 
 struct carrotRow_Previews: PreviewProvider {
     static var previews: some View {
-        carrotRow(carrot: carrots[0])
-//       carrotkRow(carrot:carrots[0])
+        carrotRow()
     }
 }
